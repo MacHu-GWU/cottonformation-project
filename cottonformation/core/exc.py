4d9b02f2033537c5580e06633211e2a4a77176f7
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-03-20 23:34:41 -0400

    update exc.py with TYPE_CHECKING

--- cottonformation/core/exc.py
+++ cottonformation/core/exc.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from .template import (
-    AWSObjectLogicIdConlictError,
-    AWSObjectNotExistsError,
-)
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+
+class AWSObjectLogicIdConflictError(Exception):
+    pass
+
+
+class AWSObjectNotExistsError(Exception):
+    @classmethod
+    def make(cls, obj_type: str, logic_id: str):
+        msg = f"Template.{obj_type} doesn't have logic id = '{logic_id}'"
+        return cls(msg)
