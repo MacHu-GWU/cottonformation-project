59a0072f955b79d87f0daae1da13885d4833b990
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-07-11 11:02:27 -0400

    add bsm for unit test

--- cottonformation/tests/boto_ses.py
+++ cottonformation/tests/boto_ses.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import boto3
+from boto_session_manager import BotoSesManager
 
 aws_profile = "aws_data_lab_sanhe"
 region = "us-east-1"
+
 boto_ses = boto3.session.Session(profile_name=aws_profile, region_name=region)
+bsm = BotoSesManager(profile_name=aws_profile, region_name=region)
+
 bucket = "aws-data-lab-sanhe-for-everything"
