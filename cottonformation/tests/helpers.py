3c710903484c2ce55376af48fa4b0c0b525be395
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-12-06 16:46:47 -0500

    add more test helper function

--- cottonformation/tests/helpers.py
+++ cottonformation/tests/helpers.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import json
+import subprocess
+
+from .paths import dir_project_root, bin_pytest, dir_htmlcov
 
 
 def jprint(data):
     print(json.dumps(data, indent=4))
+
+
+def run_cov_test(test_file: str, module: str):
+    args = [
+        bin_pytest,
+        "-s",
+        "--tb=native",
+        f"--rootdir={dir_project_root}",
+        f"--cov={module}",
+        "--cov-report",
+        "term-missing",
+        "--cov-report",
+        f"html:{dir_htmlcov}",
+        f"{test_file}",
+    ]
+    subprocess.run(args)
